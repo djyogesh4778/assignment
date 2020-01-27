bb6b1c72ccef5e6e8da47dec7d6ebda5f9c4e9e5
djyogesh4778 <yogeshdj1ga16ec180@gmail.com>
2020-01-27 13:06:26 +0530

    msg

---
+++ calculator/djy.js
@@ -0,0 +1,25 @@
+
+function add(){
+    let a=document.getElementById("data1").value
+let b=document.getElementById("data2").value
+    final1=Number(a)+Number(b)
+    alert(final1)
+}
+function sub(){
+    let a=document.getElementById("data1").value
+let b=document.getElementById("data2").value
+    final1=Number(a)-Number(b)
+    alert(final1)
+}
+function mul(){
+    let a=document.getElementById("data1").value
+let b=document.getElementById("data2").value
+    final1=Number(a)*Number(b)
+    alert(final1)
+}
+function div(){
+    let a=document.getElementById("data1").value
+let b=document.getElementById("data2").value
+    final1=Number(a)/Number(b)
+    alert(final1)
+}
